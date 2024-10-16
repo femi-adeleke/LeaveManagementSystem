2f6d668f7ca037e3badf40a4f7293ed1b1145d31
Enoch.revent <Enoch.revent@LAPTOP-QS1GTCK9>
2024-10-16 08:02:40 +0100

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Andrew Wayas",
+                DateOfBirth = new DateTime(1972, 04, 28)
+
+            };
+            return View(data);
+        }
+    }
+}
